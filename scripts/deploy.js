3e45434688564f6ef7c1bb2b5f45b5d6cb91f11b
Maxim Romanov <maxim.romanov@jetrockets.ru>
2020-08-16 18:41:55 +0300

    Added ghp

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const ghpages = require("gh-pages");
+
+ghpages.publish(
+  "__sapper__/export",
+  {
+    branch: "master",
+    repo: "https://github.com/Romanov-Maxim-traineeship/Sapper-pwa.git",
+    user: {
+      name: "Romanov-Maxim-traineeship",
+      email: "romanov.maxim.98@gmail.com",
+    },
+  },
+  () => {
+    console.log("Deploy Complete!");
+  }
+);
